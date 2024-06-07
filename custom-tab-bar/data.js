5d9b1e57af141f403f72ed882157a5fccbc676a5
赵思亮 <zhaosiliang@yunmai365.com>
2024-06-07 17:35:32 +0800

    feat: 初始项目

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'pages/home/home',
+  },
+  // {
+  //   icon: 'sort',
+  //   text: '添加',
+  //   url: 'pages/goods/category/index',
+  // },
+  // {
+  //   icon: 'cart',
+  //   text: '购物车',
+  //   url: 'pages/cart/index',
+  // },
+  {
+    icon: 'user',
+    text: '个人中心',
+    url: 'pages/usercenter/index',
+  },
+];
---
+++ envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-7glzzrwd15a09140","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
